16111d20d6f74098379275286c5373703585f823
BuffaloRob <peckham.rob@gmail.com>
2019-09-12 14:51:55 -0400

    added solution

--- excercise1.js
+++ excercise1.js
@@ -1,12 +1,25 @@
 //fill array with 60000 elements
-const list = new Array(60000).join('1.1').split('.');
+//fix code to stop stack overflow
+// const list = new Array(60000).join('1.1').split('.');
  
+// function removeItemsFromList() {
+//     var item = list.pop();
+ 
+//     if (item) {
+//         removeItemsFromList();
+//     }
+// };
+ 
+// removeItemsFromList();
+
+const list = new Array(60000).join('1.1').split('.');
+
 function removeItemsFromList() {
     var item = list.pop();
- 
+
     if (item) {
-        removeItemsFromList();
+        setTimeout(removeItemsFromList, 0);
     }
 };
- 
+
 removeItemsFromList();
