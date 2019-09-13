266d5db1bb705f2318aeb5bcc54735716ab97134
BuffaloRob <peckham.rob@gmail.com>
2019-09-13 19:44:03 -0400

    + excercise 2

---
+++ excercise2.js
@@ -0,0 +1,15 @@
+console.log(teddy)
+console.log(sing())
+console.log(anotherSong())
+
+var teddy = 'bear';
+
+// function declaration
+function sing() {
+  console.log('ohh lala la')
+}
+
+// function expression
+var anotherSong = function() {
+  console.log('singing songs')
+}
